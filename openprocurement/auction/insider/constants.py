7dc6d318161b0054de562998a496adedac3b4137
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-09-07 15:19:25 +0300

    Added new constants

--- openprocurement/auction/insider/constants.py
+++ openprocurement/auction/insider/constants.py
@@ -1,13 +1,13 @@
 from datetime import timedelta
 from decimal import Decimal
 
-
-PROCUREMENT_METHOD_TYPE = 'dgfNew'  # XXX TODO ProcurementMethodTypes for Dutch Auction
+BIDS_KEYS_FOR_COPY = ("bidder_id", "amount", "time", "dutch_winner")
+PROCUREMENT_METHOD_TYPE = 'dgfInsider'
 REQUEST_QUEUE_SIZE = -1
 REQUEST_QUEUE_TIMEOUT = 32
 # DUTCH_TIMEDELTA = timedelta(hours=5, minutes=15)
 
-DUTCH_ROUNDS = 4
+DUTCH_ROUNDS = 10
 DUTCH_DOWN_STEP = Decimal('0.01')
 MULTILINGUAL_FIELDS = ["title", "description"]
 ADDITIONAL_LANGUAGES = ["ru", "en"]
@@ -25,8 +25,8 @@ INVALIDATE_GRANT = timedelta(0, 230)
 
 DUTCH_TIMEDELTA = timedelta(minutes=1)
 FIRST_PAUSE = timedelta(seconds=5)
-FIRST_PAUSE_SECONDS = timedelta(seconds=10)
-LAST_PAUSE_SECONDS = timedelta(seconds=10)
+FIRST_PAUSE_SECONDS = timedelta(seconds=5)
+LAST_PAUSE_SECONDS = timedelta(seconds=5)
 END_DUTCH_PAUSE = timedelta(seconds=30)
 SEALEDBID_TIMEDELTA = timedelta(minutes=1)
 BESTBID_TIMEDELTA = timedelta(seconds=10)
